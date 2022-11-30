be8c395f9a75022f69bd65c40d19418226e1183c
Outspending <92287257+oShadxw@users.noreply.github.com>
2022-11-30 11:40:01 -0700

    Add files via upload

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,9 +1,4 @@
-var testing = document.getElementById('start');
 let syntax = [];
-
-testing.addEventListener('click', function() {
-    listsPost('testing', null);
-})
 
 async function test2(search) {
     const thing = document.getElementById('start');
@@ -11,10 +6,11 @@ async function test2(search) {
         thing.removeChild(thing.lastElementChild);
     }
 
-    listsPost('testing', search);
+    listsPost('start', search);
 }
 
 let timer;
+let lastResult;
 var search_bar = document.getElementById('search-bar');
 search_bar.addEventListener('keyup', function() {
     clearTimeout(timer);
@@ -27,7 +23,7 @@ search_bar.addEventListener('keyup', function() {
             thing.removeChild(thing.lastElementChild);
         }
 
-        listsPost('testing', search_bar.value);
+        listsPost('start', search_bar.value);
     }, 500);
 });
 
@@ -65,7 +61,6 @@ async function saveSyntax() {
     console.log(`finished loading all syntax!`);
 }
 
-let lastResult;
 function listsPost(postContainerElementID, search) {
     const postContainerElement = document.getElementById(postContainerElementID);
     console.log(`fetching information with filter: ${search}`);
@@ -87,13 +82,13 @@ function listsPost(postContainerElementID, search) {
 
 function postElement(post) {
     // new
-    const a = create('a', 'bg-gray-400 ml-24 mr-24 rounded-xl', null);
+    const a = create('a', 'bg-gray-300 ml-24 mr-24 rounded-xl mb-5', null);
     const div1 = create('div', 'ml-4', null);
     const div2 = create('div', 'flex float-right mr-7', null);
     // svg1
     const h1 = create('h1', 'leading-6 text-3xl mt-2 text-blue-600 font-medium', post.addon);
     const type = create('p', 'text-white mb-5 ml-2', post.type);
-    const code = create('code', 'bg-gray-600 ml-5 flex mr-5 rounded-md pb-5 pt-2 pl-2 pr-2', post.pattern);
+    const code = create('code', 'bg-black ml-5 flex mr-5 rounded-md pb-5 pt-2 pl-2 pr-2', post.pattern);
     const desc = create('p', 'mt-8 ml-7 text-gray-600', post.description);
     const button = create('button', 'mt-4 ml-3', "show more information");
 
